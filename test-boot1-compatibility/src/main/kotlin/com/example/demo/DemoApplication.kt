698476a4eb792ee03213f6f4f4da608867658ae8
cliff wu <rock1988325@gmail.com>
2019-03-07 14:19:33 +0800

    init

---
+++ test-boot1-compatibility/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,18 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+class DemoApplication {
+
+    fun main(args: Array<String>) {
+        runApplication<DemoApplication>(*args)
+    }
+
+}
